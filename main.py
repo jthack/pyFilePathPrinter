ec9422192249b3c254b6cd1773e6f01dceb29a70
Colby Thacker <34420588+ColbyThacker@users.noreply.github.com>
2018-08-11 22:07:34 -0400

    Third commit

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from file_list import FileHandler as fh
 
 
 class Gui:
+    chosen_files = []
+
     def __init__(self, master):
         self.master = master
         master.title("pyFilePathPrinter")
-
         self.label = Label(master, text="This is the pyFilePathPrinter")
         self.label.pack()
         files = fh.filter_files(fh.list_files('.'), 'py')
         for file in files:
-            #self.label = Label(master, text = numbers)
-            #self.label.pack()
             var1 = IntVar()
-            self.Checkbutton = Checkbutton(master, text=file, variable=IntVar())
+            self.Checkbutton = Checkbutton(master, text=file, variable=var1)
+            self.chosen_files.append([file, var1])
             self.Checkbutton.pack()
-
+        self.button = Button(master, text="Print", command=self.PrintOut)
+        self.button.pack()
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
-
+    def PrintOut(self):
+        for a in self.chosen_files:
+            if a[1].get() == 1:
+                print(a[0])
 def main():
   root = Tk()
   TheGui = Gui(root)
